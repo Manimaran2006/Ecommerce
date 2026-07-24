5f0cf282c130ea9d098074e964d30436be8f384b
Manimaran P <152673529+Manimaran2006@users.noreply.github.com>
2026-07-24 18:17:52 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,51 +1,24 @@
-import { Fragment, useEffect, useState } from 'react';
-import ProductCard from '../components/ProductCard';
+import {Fragment, useEffect, useState} from 'react'
+import ProductCard from '../components/ProductCard'
 import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
-
     const [products, setProducts] = useState([]);
-    const [searchParams] = useSearchParams();
+    const [searchParams, setSearchParams] =  useSearchParams()
 
     useEffect(() => {
+        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
+        .then(res => res.json())
+        .then( res => setProducts(res.products))
+    },[searchParams])
 
-        fetch(`http://98.93.119.171:8000/api/v1/products?${searchParams}`)
-            .then(res => res.json())
-            .then(res => {
-                setProducts(res.products);
-            })
-            .catch(error => {
-                console.log(error);
-            });
+    return <Fragment>
+        <h1 id="products_heading">Latest Products</h1>
 
-    }, [searchParams]);
-
-
-    return (
-        <Fragment>
-
-            <h1 id="products_heading">
-                Latest Products
-            </h1>
-
-
-            <section id="products" className="container mt-5">
-
-                <div className="row">
-
-                    {
-                        products.map(product => (
-                            <ProductCard 
-                                key={product._id}
-                                product={product}
-                            />
-                        ))
-                    }
-
-                </div>
-
-            </section>
-
-        </Fragment>
-    )
+        <section id="products" className="container mt-5">
+        <div className="row">
+            {products.map(product =><ProductCard product={product} />)} 
+        </div>
+        </section>
+    </Fragment>
 }
