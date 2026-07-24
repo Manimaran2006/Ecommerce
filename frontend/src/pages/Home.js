e90f4e2fdf29f49b0df01809bf576e460f3701dd
Manimaran P <152673529+Manimaran2006@users.noreply.github.com>
2026-07-24 18:08:44 +0530

    Update Home.js

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,24 +1,51 @@
-import {Fragment, useEffect, useState} from 'react'
-import ProductCard from '../components/ProductCard'
+import { Fragment, useEffect, useState } from 'react';
+import ProductCard from '../components/ProductCard';
 import { useSearchParams } from 'react-router-dom';
 
 export default function Home() {
+
     const [products, setProducts] = useState([]);
-    const [searchParams, setSearchParams] =  useSearchParams()
+    const [searchParams] = useSearchParams();
 
     useEffect(() => {
-        fetch(process.env.REACT_APP_API_URL+'/products?'+searchParams)
-        .then(res => res.json())
-        .then( res => setProducts(res.products))
-    },[searchParams])
 
-    return <Fragment>
-        <h1 id="products_heading">Latest Products</h1>
+        fetch(`http://98.93.119.171:8000/api/v1/products?${searchParams}`)
+            .then(res => res.json())
+            .then(res => {
+                setProducts(res.products);
+            })
+            .catch(error => {
+                console.log(error);
+            });
 
-        <section id="products" className="container mt-5">
-        <div className="row">
-            {products.map(product =><ProductCard product={product} />)} 
-        </div>
-        </section>
-    </Fragment>
+    }, [searchParams]);
+
+
+    return (
+        <Fragment>
+
+            <h1 id="products_heading">
+                Latest Products
+            </h1>
+
+
+            <section id="products" className="container mt-5">
+
+                <div className="row">
+
+                    {
+                        products.map(product => (
+                            <ProductCard 
+                                key={product._id}
+                                product={product}
+                            />
+                        ))
+                    }
+
+                </div>
+
+            </section>
+
+        </Fragment>
+    )
 }
